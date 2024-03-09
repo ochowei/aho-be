16ea31cdbb7adc96328a5fee22ab74c844b86aac
William <ochowei@gmail.com>
2024-03-09 22:42:15 +0800

    feat: post login

--- routes/api/login.js
+++ routes/api/login.js
@@ -6,4 +6,11 @@ router.get("/v1/", (req, res, next) => {
   res.send({ user_id: "123", nickname: "mock-user", email: "mock@mock.org" });
 });
 
+router.post("/v1/", (req, res, next) => {
+  const { email, password } = req.body;
+  console.log("email", email);
+  console.log("password", password);
+  res.send({ user_id: "123", nickname: "mock-user", email: email });
+});
+
 module.exports = router;
