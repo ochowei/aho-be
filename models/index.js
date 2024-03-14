b40c00e1669eab6450e43d0ddc01dd99764009e0
William <ochowei@gmail.com>
2024-03-14 11:41:47 +0800

    fix: unused tables

--- models/index.js
+++ models/index.js
@@ -1,19 +1,13 @@
 const { setupUser, User } = require('./User');
-const { setupIdentity, Identity } = require('./Identity');
-const { setupUserMetadata, UserMetadata } = require('./UserMetadata');
 const { setupUserAuth, UserAuth } = require('./UserAuth');
 
 const setupModels = async (sequelize) => {
   await setupUser(sequelize);
-  await setupIdentity(sequelize);
-  await setupUserMetadata(sequelize);
   await setupUserAuth(sequelize);
 };
 
 const models = {
   User,
-  Identity,
-  UserMetadata,
   UserAuth,
 };
 
