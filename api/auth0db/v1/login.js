cf09e437ce6906fa4cfa1e77ae90b87af291929d
William <ochowei@gmail.com>
2024-06-25 01:29:51 +0800

    update routes

--- api/auth0db/v1/login.js
+++ api/auth0db/v1/login.js
@@ -5,12 +5,19 @@ const { handleResponse } = require('../../../services/common/response');
 const service = require('../../../services/auth0db/service');
 const { checkAuth0db, checkEmail } = require('../../../middlewares/common/handler');
 
-router.all(
+router.put(
   '/api/auth0db/v1/login',
   checkAuth0db,
   checkEmail,
   service.login,
   handleResponse,
+);
+
+router.options(
+  '/api/auth0db/v1/login',
+  (req, res) => {
+    res.send();
+  },
 );
 
 const app = express();
