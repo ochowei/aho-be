06cfc8791ba67a6ac7559bffd1382e72bae38a58
William <ochowei@gmail.com>
2024-06-25 11:27:24 +0800

    update mysql setup

--- api/auth0db/v1/create.js
+++ api/auth0db/v1/create.js
@@ -5,6 +5,20 @@ const { handleResponse } = require('../../../services/common/response');
 const service = require('../../../services/auth0db/service');
 const { checkAuth0db, checkPassword, checkEmail } = require('../../../middlewares/common/handler');
 
+const { initSequelize } = require('../../../connections/mysql');
+
+const mysqlConfig = {
+  master: {
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    ca: process.env.MYSQL_CA,
+  },
+};
+initSequelize(mysqlConfig); // TODO: error handle
+
 router.post(
   '/api/auth0db/v1/create',
   checkAuth0db,
